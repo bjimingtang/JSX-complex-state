b0489046e83d9689d88e7da1765afa52f85cf6fd
bjimingtang <brendan.jiming.tang@gmail.com>
2020-12-18 15:07:38 -0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function App() {
+  const [fullName, setFullName] = useState({
+    firstName: "",
+    lastName: ""
+  });
+  function updateFullName(event) {
+    const { value, name } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          firstName: value,
+          lastName: prevValue.lastName
+        };
+      } else if (name === "lName") {
+        return {
+          firstName: prevValue.firstName,
+          lastName: value
+        };
+      }
+    });
+  }
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.firstName} {fullName.lastName}
+      </h1>
+      <form>
+        <input
+          name="firstName"
+          onChange={updateFullName}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          name="lastName"
+          onChange={updateFullName}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
